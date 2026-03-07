579818ada3a4916a1b7d894bf3dba72cd64c5b36
piber06 <ericpiberr@gmail.com>
2026-03-07 20:26:57 +0100

    starting lab5

---
+++ src/framework/material.h
@@ -0,0 +1,13 @@
+
+#include "shader.h"
+#include "texture.h"
+#include "framework.h"
+
+class Material
+{
+public:
+    Shader *shader;
+    Texture *texture;
+    Color *components;
+    // Shininess....
+}
