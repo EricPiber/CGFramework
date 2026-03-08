fe6f5e928eeb552b9652d5f0d49d6972cb8efa8d
piber06 <ericpiberr@gmail.com>
2026-03-08 01:26:43 +0100

    DONT MOVE THINGS TO INIT → ERROR

--- src/framework/application.cpp
+++ src/framework/application.cpp
@@ -151,7 +151,6 @@ void Application::Init(void)
         shader = new Shader();
         mesh->CreateQuad();
         shader = Shader::Get("shaders/quad.vs", "shaders/quad.fs");
-        shader->SetFloat("u_pi", PI);
         texture = Texture::Get("images/fruits.png");
         
         Mesh *mesh0 = new Mesh();
@@ -230,6 +229,7 @@ void Application::Render(void)
             shader->SetVector2("u_resolution", Vector2((float)window_width,(float)window_height));
             shader->SetInt("u_task", task);
             shader->SetFloat("u_aspect", (float)window_width/(float)window_height);
+            shader->SetFloat("u_pi", PI);
             shader->SetInt("u_exercise", exercise);
             shader->SetTexture("u_texture", texture);
             shader->SetFloat("u_time", time);
